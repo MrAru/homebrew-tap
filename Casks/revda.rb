9b971d8e39d6141776a5ba9449ca733a0fe0749f
Jin-Bo Fu <jinbo.fu@qpalpha.com>
2026-03-23 14:29:11 +0800

    update

--- Casks/revda.rb
+++ Casks/revda.rb
@@ -1,15 +1,15 @@
 # Casks/revda.rb
 cask "revda" do
-  version "2026.03.20-8aa4319"
-  sha256 "6f4c4e0fa244d92d109547c3d40c4d982ceed2880488866f99434fd0d3b6d46c"
+  version "2026.03.23-718c5bc"
+  sha256 "fa1a5184be6d9090776b9f808f648422f8b61bcb843ed7e97e71d108025dc214"
 
-  url "https://github.com/MrAru/revda-macos-build/releases/download/#{version}/revda_5.2.0_aarch64.dmg"
+  url "https://github.com/MrAru/revda-macos-build/releases/download/#{version}/Revda-macOS-aarch64.dmg"
   name "Revda"
   desc "Revda media player"
   homepage "https://github.com/MrAru/revda-macos-build"
 
   depends_on formula: "ffmpeg"
-  depends_on cask: "mpv"
+  depends_on formula: "mpv"
   
   app "revda.app"
   binary "#{appdir}/revda.app/Contents/MacOS/dmlive"
